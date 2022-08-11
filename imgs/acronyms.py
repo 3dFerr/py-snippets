c826ca32b6cd8d85f99318e51e543f6c3e60eddc
pauloferrti <pauloferrti@gmail.com>
2022-08-11 18:59:50 -0300

    c6 - create acronym funcionality

--- imgs/acronyms.py
+++ imgs/acronyms.py
@@ -1,3 +1,8 @@
 # A simple program to make an acronym from a phrase
 
 phrase = input("Enter a Phrase: ")
+text = phrase.split()
+Acronyms = " "
+for i in text:
+    Acronyms = Acronyms+str(i[0]).upper()
+print(Acronyms)
